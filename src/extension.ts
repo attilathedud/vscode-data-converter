4a6375e52a2106c3738360885569de0cbc03a75c
attilathedud <edips@protonmail.com>
2019-10-06 21:45:26 -0500

    Adding variable padding

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,97 @@ import * as vscode from "vscode";
 
 import { converter } from "./converter";
 
+function beginParse(menu_selection:vscode.QuickPickItem, quick_options_hash:{}, paddingLength: number) {
+    const editor = vscode.window.activeTextEditor;
+
+    editor.edit(function (edit) {
+        editor.selections.forEach(element => {
+            let selected_text: string = editor.document.getText(new vscode.Range(element.start, element.end));
+
+            if (selected_text.length == 0) {
+                return;
+            }
+
+            let should_prepend_with_identifier: boolean =
+                vscode.workspace.getConfiguration('converter').get('prependDataWithIdentifier');
+
+            let spaces_indicate_delimiter: boolean =
+                vscode.workspace.getConfiguration('converter').get('treatSpacesAsDelimiter');
+
+            let selected_text_segments: string[] = selected_text.split('\n');
+            let segmented_text: string = '';
+
+            selected_text_segments.forEach(segment => {
+                let segments_within_line: string[] = new Array(segment);
+
+                if (menu_selection.label == quick_options_hash['decimal_to_binary'].label ||
+                    menu_selection.label == quick_options_hash['decimal_to_hex'].label ||
+                    menu_selection.label == quick_options_hash['hex_to_binary'].label ||
+                    menu_selection.label == quick_options_hash['hex_to_decimal'].label ||
+                    menu_selection.label == quick_options_hash['binary_to_decimal'].label ||
+                    menu_selection.label == quick_options_hash['binary_to_hex'].label) {
+
+                    if (!spaces_indicate_delimiter) {
+                        if (!parseInt(segment.replace(new RegExp(' ', 'g'), ''))) {
+                            segmented_text += segment + '\n';
+                            return;
+                        }
+
+                        segment = segment.replace(new RegExp(' ', 'g'), '');
+                    }
+
+                    segments_within_line = segment.split(' ');
+                }
+
+                segments_within_line.forEach(line_segment => {
+                    switch (menu_selection.label) {
+                        case quick_options_hash['decimal_to_binary'].label:
+                            segmented_text += converter.convert_text_to_base(line_segment, 10, 2, paddingLength, should_prepend_with_identifier);
+                            break;
+                        case quick_options_hash['decimal_to_hex'].label:
+                            segmented_text += converter.convert_text_to_base(line_segment, 10, 16, paddingLength, should_prepend_with_identifier);
+                            break;
+                        case quick_options_hash['hex_to_binary'].label:
+                            segmented_text += converter.convert_text_to_base(line_segment, 16, 2, paddingLength, should_prepend_with_identifier);
+                            break;
+                        case quick_options_hash['hex_to_decimal'].label:
+                            segmented_text += converter.convert_text_to_base(line_segment, 16, 10, paddingLength, should_prepend_with_identifier);
+                            break;
+                        case quick_options_hash['binary_to_decimal'].label:
+                            segmented_text += converter.convert_text_to_base(line_segment, 2, 10, paddingLength, should_prepend_with_identifier);
+                            break;
+                        case quick_options_hash['binary_to_hex'].label:
+                            segmented_text += converter.convert_text_to_base(line_segment, 2, 16, paddingLength, should_prepend_with_identifier);
+                            break;
+                        case quick_options_hash['escape_url'].label:
+                            segmented_text += encodeURIComponent(line_segment);
+                            break;
+                        case quick_options_hash['unescape_url'].label:
+                            segmented_text += decodeURIComponent(line_segment);
+                            break;
+                        case quick_options_hash['unicode_to_hex'].label:
+                            segmented_text += converter.unicode_to_hex(line_segment);
+                            break;
+                        case quick_options_hash['hex_to_unicode'].label:
+                            segmented_text += converter.hex_to_unicode(line_segment);
+                            break;
+                    }
+
+                    segmented_text += ' ';
+                });
+
+                segmented_text = segmented_text.slice(0, -1);
+
+                segmented_text += '\n';
+            });
+
+            segmented_text = segmented_text.slice(0, -1);
+
+            edit.replace(element, segmented_text);
+        });
+    });
+}
+
 export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('extension.dataConversion', () => {
         if (!vscode.window.activeTextEditor) {
@@ -35,98 +126,27 @@ export function activate(context: vscode.ExtensionContext) {
             if (!menu_selection) {
                 return;
             }
-
-            const editor = vscode.window.activeTextEditor;
-
-            editor.edit(function (edit) {
-                editor.selections.forEach(element => {
-                    let selected_text: string = editor.document.getText(new vscode.Range(element.start, element.end));
-
-                    if (selected_text.length == 0) {
-                        return;
+          
+            let variablePadding: boolean = 
+                vscode.workspace.getConfiguration('converter').get('useVariablePadding');
+            
+            if(variablePadding) {
+                vscode.window.showInputBox({ "prompt" : "Padding to apply", "placeHolder": "0"}).then((padding) => {
+                    let padding_number: number = Number(padding);
+                    if(isNaN(padding_number)){
+                        vscode.window.showInformationMessage('Please enter a number.');
                     }
-
-                    let should_prepend_with_identifier: boolean =
-                        vscode.workspace.getConfiguration('converter').get('prependDataWithIdentifier');
-
-                    let spaces_indicate_delimiter: boolean =
-                        vscode.workspace.getConfiguration('converter').get('treatSpacesAsDelimiter');
-
-                    let paddingLength: number = 
-                        vscode.workspace.getConfiguration('converter').get('padToLength');
-
-                    let selected_text_segments: string[] = selected_text.split('\n');
-                    let segmented_text: string = '';
-
-                    selected_text_segments.forEach(segment => {
-                        let segments_within_line: string[] = new Array(segment);
-
-                        if (menu_selection.label == quick_options_hash['decimal_to_binary'].label ||
-                            menu_selection.label == quick_options_hash['decimal_to_hex'].label ||
-                            menu_selection.label == quick_options_hash['hex_to_binary'].label ||
-                            menu_selection.label == quick_options_hash['hex_to_decimal'].label ||
-                            menu_selection.label == quick_options_hash['binary_to_decimal'].label ||
-                            menu_selection.label == quick_options_hash['binary_to_hex'].label) {
-
-                            if (!spaces_indicate_delimiter) {
-                                if (!parseInt(segment.replace(new RegExp(' ', 'g'), ''))) {
-                                    segmented_text += segment + '\n';
-                                    return;
-                                }
-
-                                segment = segment.replace(new RegExp(' ', 'g'), '');
-                            }
-
-                            segments_within_line = segment.split(' ');
-                        }
-
-                        segments_within_line.forEach(line_segment => {
-                            switch (menu_selection.label) {
-                                case quick_options_hash['decimal_to_binary'].label:
-                                    segmented_text += converter.convert_text_to_base(line_segment, 10, 2, paddingLength, should_prepend_with_identifier);
-                                    break;
-                                case quick_options_hash['decimal_to_hex'].label:
-                                    segmented_text += converter.convert_text_to_base(line_segment, 10, 16, paddingLength, should_prepend_with_identifier);
-                                    break;
-                                case quick_options_hash['hex_to_binary'].label:
-                                    segmented_text += converter.convert_text_to_base(line_segment, 16, 2, paddingLength, should_prepend_with_identifier);
-                                    break;
-                                case quick_options_hash['hex_to_decimal'].label:
-                                    segmented_text += converter.convert_text_to_base(line_segment, 16, 10, paddingLength, should_prepend_with_identifier);
-                                    break;
-                                case quick_options_hash['binary_to_decimal'].label:
-                                    segmented_text += converter.convert_text_to_base(line_segment, 2, 10, paddingLength, should_prepend_with_identifier);
-                                    break;
-                                case quick_options_hash['binary_to_hex'].label:
-                                    segmented_text += converter.convert_text_to_base(line_segment, 2, 16, paddingLength, should_prepend_with_identifier);
-                                    break;
-                                case quick_options_hash['escape_url'].label:
-                                    segmented_text += encodeURIComponent(line_segment);
-                                    break;
-                                case quick_options_hash['unescape_url'].label:
-                                    segmented_text += decodeURIComponent(line_segment);
-                                    break;
-                                case quick_options_hash['unicode_to_hex'].label:
-                                    segmented_text += converter.unicode_to_hex(line_segment);
-                                    break;
-                                case quick_options_hash['hex_to_unicode'].label:
-                                    segmented_text += converter.hex_to_unicode(line_segment);
-                                    break;
-                            }
-
-                            segmented_text += ' ';
-                        });
-
-                        segmented_text = segmented_text.slice(0, -1);
-
-                        segmented_text += '\n';
-                    });
-
-                    segmented_text = segmented_text.slice(0, -1);
-
-                    edit.replace(element, segmented_text);
+                    else {
+                        beginParse(menu_selection, quick_options_hash, padding_number);
+                    }
                 });
-            });
+            }
+            else {
+                let paddingLength: number = 
+                    vscode.workspace.getConfiguration('converter').get('padToLength');
+                
+                beginParse(menu_selection, quick_options_hash, paddingLength);
+            }
         });
     });
 
